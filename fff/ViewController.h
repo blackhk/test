2834e86cd960018c3a92774f013291172183308f
Artem Pomelnikov <artempomelnikov@Mac-mini-Artem.local>
2014-07-09 18:32:49 +0400

    Initial Commit

---
+++ fff/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fff
+//
+//  Created by Artem Pomelnikov on 09.07.14.
+//  Copyright (c) 2014 Artem Pomelnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fff/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fff
+//
+//  Created by Artem Pomelnikov on 09.07.14.
+//  Copyright (c) 2014 Artem Pomelnikov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
