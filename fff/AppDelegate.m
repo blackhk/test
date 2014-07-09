44222957e9bc3b3ee7afb7aec5fedb343fa68569
Artem Pomelnikov <artempomelnikov@Mac-mini-Artem.local>
2014-07-09 18:42:38 +0400

    проверено

--- fff/AppDelegate.m
+++ fff/AppDelegate.m
@@ -20,6 +20,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+     NSLog(@"application did finish launching");
     return YES;
 }
 
